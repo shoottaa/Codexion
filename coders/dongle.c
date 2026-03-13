15d188177192e20a3c168dcab4563146330f5b97
shotta <erwan.gdc49@gmail.com>
2026-03-13 11:16:50 +0100

    use timedewait

--- coders/dongle.c
+++ coders/dongle.c
@@ -25,6 +25,7 @@ int	init_dongles(t_global *sim)
 		pthread_mutex_init(&sim->dongles[i].mutex, NULL);
 		pthread_cond_init(&sim->dongles[i].cond, NULL);
 		sim->dongles[i].waiters = pq_create(sim->args.num_coders);
+		pthread_cond_broadcast(&sim->dongles[i].cond);
 		i++;
 	}
 	return (0);
@@ -46,19 +47,35 @@ int	destroy_dongles(t_global *sim)
 	return (0);
 }
 
-void	take_one_dongle(t_coder *coder, t_dongle *dongle)
+// nsec = nanoseconds (1000000000 nsec = 1 sec)
+// usec = microseconds
+void    take_one_dongle(t_coder *coder, t_dongle *dongle)
 {
-	pthread_mutex_lock(&dongle->mutex);
-	if (coder->global->args.scheduler == 1)
-		pq_push(dongle->waiters, coder, coder->deadline);
-	else
-		pq_push(dongle->waiters, coder, get_time_ms());
-	while (dongle->is_used || get_time_ms() < dongle->cooldown
-		|| dongle->waiters->node[0].coder != coder)
-		pthread_cond_wait(&dongle->cond, &dongle->mutex);
-	pq_pop(dongle->waiters);
-	dongle->is_used = 1;
-	pthread_mutex_unlock(&dongle->mutex);
+    struct timeval		tv;
+    struct timespec   	ts;
+
+    pthread_mutex_lock(&dongle->mutex);
+    if (coder->global->args.scheduler == 1)
+        pq_push(dongle->waiters, coder, coder->deadline);
+    else
+        pq_push(dongle->waiters, coder, get_time_ms());
+    while (dongle->is_used || get_time_ms() < dongle->cooldown
+        || (dongle->waiters->size > 0
+            && dongle->waiters->node[0].coder != coder))
+    {
+        gettimeofday(&tv, NULL);
+        ts.tv_sec = tv.tv_sec;
+        ts.tv_nsec = (tv.tv_usec + 2000) * 1000;
+        if (ts.tv_nsec >= 1000000000)
+        {
+            ts.tv_sec += 1;
+            ts.tv_nsec -= 1000000000;
+        }
+        pthread_cond_timedwait(&dongle->cond, &dongle->mutex, &ts);
+    }
+    pq_pop(dongle->waiters);
+    dongle->is_used = 1;
+    pthread_mutex_unlock(&dongle->mutex);
 }
 
 void	release_dongles(t_coder *coder)
