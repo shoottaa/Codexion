2910a5c1d7b6fd5d5853230af63468ff8c4e269c
shotta <erwan.gdc49@gmail.com>
2026-04-02 18:07:45 +0200

    fix keyboard input memory leaks

--- coders/main.c
+++ coders/main.c
@@ -10,14 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include "main.h"
+
+static void	handle_sigint(int sig)
+{
+	static t_global	*g_sim = NULL;
+
+	(void)sig;
+	if (g_sim)
+		g_sim->flag = 1;
+}
 
 int	main(int argc, char **argv)
 {
-	t_global	sim;
+	t_global			sim;
+	struct sigaction	sa;
 
 	if (check_args(argc, argv))
 		return (1);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handle_sigint;
+	sigaction(SIGINT, &sa, NULL);
 	init_sim(&sim, argv);
 	return (0);
 }
